9edecc5aef12bca988f2a1f5d7e3d386f6cf48df
Davi Alves <cadwvk@gmail.com>
2025-01-08 12:42:43 -0300

    refactor styles and update homepage structure; integrate Tailwind CSS and improve typography

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,24 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      height: {
+        'full-svh': '100svh',
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
       },
     },
   },
   plugins: [],
-} satisfies Config;
+} satisfies Config
