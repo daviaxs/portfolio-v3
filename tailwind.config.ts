1ed8b6827cd3e68be051bf748183816e64a8d111
Davi Alves <cadwvk@gmail.com>
2025-03-01 21:54:30 -0300

    refactor Tailwind CSS configuration to add gray-light and gray-dark color shades

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,8 +14,22 @@ export default {
         background: 'var(--background)',
         foreground: 'var(--foreground)',
 
-        zinc: {
-          200: '#E8E8E8',
+        'gray-light': {
+          'gray-light-50': '#F2F2F7',
+          'gray-light-100': '#E5E5EA',
+          'gray-light-200': '#D1D1D6',
+          'gray-light-300': '#C7C7CC',
+          'gray-light-400': '#AEAEB2',
+          'gray-light-500': '#8E8E93',
+        },
+
+        'gray-dark': {
+          'gray-dark-50': '#1C1C1E',
+          'gray-dark-100': '#232325',
+          'gray-dark-200': '#3A3A3C',
+          'gray-dark-300': '#48484A',
+          'gray-dark-400': '#636366',
+          'gray-dark-500': '#8E8E93',
         },
       },
       fontFamily: {
