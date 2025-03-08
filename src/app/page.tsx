9f7ca1a914c21da87f9d0dca72b2f4b958073790
Davi Alves <cadwvk@gmail.com>
2025-03-08 01:18:28 -0300

    feat: add avatar image and user information to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,6 +19,27 @@ export default function HomePage() {
             quality={100}
           />
         </div>
+        <div className="z-10 flex w-full h-fit gap-6 px-8">
+          <Image
+            src="/imgs/avatar.png"
+            width={160}
+            height={160}
+            alt="avatar"
+            quality={100}
+            className="rounded-full border-4 border-white dark:border-gray-dark-50 shadow-[0px_12px_16px_-4px_rgba(0,0,0,0.08)]"
+          />
+
+          <div className="flex pt-16">
+            <div className="flex flex-col">
+              <p className="text-display-sm font-semibold text-gray-dark-50 dark:text-gray-light-50">
+                Davi Alves
+              </p>
+              <p className="text-md font-normal text-gray-dark-400 dark:text-gray-light-500">
+                São Paulo, Brasil
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   )
