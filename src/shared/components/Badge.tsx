bb7dda8aea9582e5b674181953bed2725ea72b41
Davi Alves <cadwvk@gmail.com>
2025-03-13 15:52:33 -0300

    feat: add Badge component for displaying icons with text

---
+++ src/shared/components/Badge.tsx
@@ -0,0 +1,16 @@
+import { ReactElement } from 'react'
+
+interface BadgeProps {
+  icon: ReactElement
+  text: string
+}
+
+export function Badge({ icon, text }: BadgeProps) {
+  return (
+    <div className="flex items-center justify-center h-9 gap-2 px-4 rounded-medium bg-gray-light-50 dark:bg-gray-dark-100 text-gray-dark-50 dark:text-gray-light-50 border border-gray-light-100 dark:border-gray-dark-200 shadow-badge">
+      {icon}
+
+      <p>{text}</p>
+    </div>
+  )
+}
