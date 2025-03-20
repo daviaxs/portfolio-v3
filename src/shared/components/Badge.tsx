976c18181b3a72a35bbd2355e36f61c4afc8c381
Davi Alves <cadwvk@gmail.com>
2025-03-20 16:50:44 -0300

    feat: update badge component shadow for improved styling

--- src/shared/components/Badge.tsx
+++ src/shared/components/Badge.tsx
@@ -7,7 +7,7 @@ interface BadgeProps {
 
 export function Badge({ icon, text }: BadgeProps) {
   return (
-    <div className="flex items-center justify-center select-none h-9 gap-2 px-4 rounded-medium bg-gray-light-50 dark:bg-gray-dark-100 text-gray-dark-50 dark:text-gray-light-50 border border-gray-light-100 dark:border-gray-dark-200 shadow-badge">
+    <div className="flex items-center justify-center select-none h-9 gap-2 px-4 rounded-medium bg-gray-light-50 dark:bg-gray-dark-100 text-gray-dark-50 dark:text-gray-light-50 border border-gray-light-100 dark:border-gray-dark-200 shadow-1">
       {icon}
 
       <p>{text}</p>
