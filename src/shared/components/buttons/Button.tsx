f52bf237797abcf9a2516a06bfb7c61099242946
Davi Alves <cadwvk@gmail.com>
2025-03-20 11:09:42 -0300

    feat: enhance Button component with improved styling and hover effects

--- src/shared/components/buttons/Button.tsx
+++ src/shared/components/buttons/Button.tsx
@@ -6,7 +6,7 @@ interface ButtonProps extends HTMLProps<HTMLButtonElement> {
 
 export function Button({ children }: ButtonProps) {
   return (
-    <button className="flex items-center justify-center px-4 h-[40px] rounded-medium bg-red-50">
+    <button className="flex items-center justify-center px-4 gap-2 h-[40px] rounded-medium bg-red-50 text-gray-light-50 hover:bg-red-100 transition-colors">
       {children}
     </button>
   )
